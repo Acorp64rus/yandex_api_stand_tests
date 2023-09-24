978bbf698dbcd8100baa754307763d23f768847d
Acorp64rus <acorp64rus@yandex.ru>
2023-09-24 15:50:43 +0400

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://8e8d35ec-e3dc-4c65-962e-3e4419a40411.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
